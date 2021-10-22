bc27d95ef888a7b356d73daf32612cec39648762
Dark-Tower-Coder <tower@posteo.de>
2021-10-22 23:58:32 +0200

    demo application completed

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hello-world-spring_boot-angularjs
-first try with spring boot and angularjs
+With this little program I would like to gain my first experience with Spring boot as backend and AngularJS as frontend.
+
+For this I have the instructions from
+https://www.baeldung.com/spring-boot-angular-web
+used and necessary adjustments made in order to be able to start the application completely.
--- src/main/java/de/towersweb/helloworld/helloworldspring_bootangularjs/controller/UserController.java
+++ src/main/java/de/towersweb/helloworld/helloworldspring_bootangularjs/controller/UserController.java
@@ -4,6 +4,8 @@ import de.towersweb.helloworld.helloworldspring_bootangularjs.entity.User;
 import de.towersweb.helloworld.helloworldspring_bootangularjs.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +16,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
+    @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/users")
